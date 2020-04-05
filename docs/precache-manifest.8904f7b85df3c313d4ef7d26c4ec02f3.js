d48a915944c2b6da1cd1d4b0bd60496fb1cd5d87
David <davidghazaryan2000@gmail.com>
2020-04-06 02:32:59 +0400

    docs update

---
+++ docs/precache-manifest.8904f7b85df3c313d4ef7d26c4ec02f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0695037f9452404b3e16ce2808bec55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80cf1674b9e56224908d",
+    "url": "/static/css/main.4421564f.chunk.css"
+  },
+  {
+    "revision": "fb536641eb9084ce5c37",
+    "url": "/static/js/2.9d958aa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d958aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cf1674b9e56224908d",
+    "url": "/static/js/main.c5409010.chunk.js"
+  },
+  {
+    "revision": "8634a711f4dc8df9cf9b",
+    "url": "/static/js/runtime-main.7c217db9.js"
+  }
+]);

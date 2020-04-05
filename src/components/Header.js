e8441b363ed235ca2a565de4c944ee761ee3e174
David <davidghazaryan2000@gmail.com>
2020-04-05 21:20:30 +0400

    Added components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/Header.scss';
+
+function Header() {
+    return (
+        <header className="App-header">
+            {/*<h1>Ap jan.</h1>*/}
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem ap jan?"/>
+            <p>Meme Generator</p>
+        </header>
+    );
+}
+
+export default Header;
